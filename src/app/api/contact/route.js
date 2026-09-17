636a3d07e2060fa65cfa68d2a84a6eea833e9bbd
Yasaman <yasamanmirazei1997@gmail.com>
2026-09-17 13:56:30 +0330

    fixed resend api key envro var

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -3,7 +3,7 @@
 const { Resend } = require("resend");
 
 
-const resend = new Resend(process.env.RESEND_API_KEy)
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(request) {
     try {
